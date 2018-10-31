a04fc5b0dd4495df870413294815484563e05b3b
Andriiuxa <andriuska96@gmail.com>
2018-10-31 14:55:26 +0000

    welcome screen sorted

--- job-search/screens/WelcomeScreen.js
+++ job-search/screens/WelcomeScreen.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+    { text: 'Welcome to JobApp', color: '#03A9F4' },
+    { text: 'An app that will help you to find a job', color: '#009688' },
+    { text: 'If you think you are ready, we can beggin.', color: '#03A9F4' }
+];
 
 class WelcomeScreen extends Component {
+    onSlidesComplete = () => {
+        this.props.navigation.navigate('auth');
+    }
+    
+
     render() {
         return (
-            <View>
-                <Text>WelcomeScreen!</Text>
-                <Text>WelcomeScreen!</Text>
-                <Text>WelcomeScreen!</Text>
-                <Text>WelcomeScreen!</Text>
-                <Text>WelcomeScreen!</Text>
-                <Text>WelcomeScreen!</Text>
-            </View>
-        )
+            <Slides 
+                data={SLIDE_DATA} 
+                onComplete={this.onSlidesComplete}      
+            />
+        );
     }
 }
 
