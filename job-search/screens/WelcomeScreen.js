acdb76df4225930677a7697dd1b9b48961abc16a
Andriiuxa <andriuska96@gmail.com>
2018-10-31 20:09:29 +0000

    add facebook login

--- job-search/screens/WelcomeScreen.js
+++ job-search/screens/WelcomeScreen.js
@@ -5,7 +5,7 @@ import Slides from '../components/Slides';
 const SLIDE_DATA = [
     { text: 'Welcome to JobApp', color: '#03A9F4' },
     { text: 'An app that will help you to find a job', color: '#009688' },
-    { text: 'If you think you are ready, we can beggin.', color: '#03A9F4' }
+    { text: 'If you think you are ready, we can begin.', color: '#03A9F4' }
 ];
 
 class WelcomeScreen extends Component {
